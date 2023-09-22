3f0871b541a3a8d14a6cd8b4689451c720faabbe
Iamneo <techadmin@iamneo.ai>
2023-09-22 11:28:11 +0000

    Backup Commit

--- ec2_instance/ec2.tf
+++ ec2_instance/ec2.tf
@@ -5,7 +5,15 @@ terraform {
         }
     }
 }
-provider "aws"
+provider "aws"{
+    region = "ap-southeast-1"
+    access_key= "AKIAUELQHKQHXIUI5PNP"
+    secret_key= "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"
+}
+resource "aw_instance" "aws"{
+    ami=""
+    instance_type='
+}
     
   required_version = ">= 0.12"
 }
